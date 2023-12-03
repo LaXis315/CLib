24fe031466f0a65312c83b01145cf6b5f56eb1ab
LaXis315 <lorenzo.tilli.w@gmail.com>
2023-12-03 19:47:39 +0100

    Corrected some bugs and syntax errors

--- tree.h
+++ tree.h
@@ -1,66 +1,61 @@
 #include <malloc.h>
-#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 //struttura della lista
 
-typedef struct node{  //nodi collegabili con il successivo e precendente
-	struct node *parent;
+typedef struct Node Node;
+struct Node{  //nodi collegabili con il successivo e precendente
+	Node *parent;
 	int n_childrens;
-	struct node **childrens;
+	Node **childrens;
 	int key;
 	char *nome;
+};
 
-} Node;
 
-
-typedef struct head{  //testa che punta alla radice dell' albero e la sua grandezza
+typedef struct{  //testa che punta alla radice dell' albero e la sua grandezza
 	int size;
 	Node *root;  //puntatore alla radice
-} Head;
+} Tree;
 
 
 //Metodi per inizializzare, incrementare, cercare, rimuovere
-
-Head *mk_head();
-int head_size(Head *tree);
 Node *mk_node(int key);
-Node *rec_get(Node *root, int key);
-void incr_list(Head *tree);
-int del_node(Head *tree, int key);
-int del_head(Head *tree);
 
 
 
 
-Head *mk_head(int root_key){
+Tree *mk_tree(int root_key){
 
-	Head *head = (Head*) malloc(sizeof(Head));
-	head->size = 1;
-	head->root = mk_node(root_key);
-	return head;
+	Tree *tree_head = (Tree*) malloc(sizeof(Tree));
+	tree_head->size = 1;
+	tree_head->root = mk_node(root_key);
+	return tree_head;
 
 }
 
-Node *mk_node(Node* parent,int key){
+Node *mk_node(int key){
 	Node *node = (Node*) malloc(sizeof(Node));
-	node -> parent = parent;
-	node -> childrens = (Node *) malloc(0);
+	node -> parent = NULL;
+	node -> childrens = (Node **) malloc(0);
 	node -> key = key;
 	return node;
 }
 
 /******************************************************************/
 
-int head_size(Head *tree){
+int tree_size(Tree *tree){
 	return tree -> size;
 }
 
 Node *rec_get(Node *root, int key){
-	if(node->key == key)
-		return node;
-	int n_child;	
+	int n_child;
+	if(root->key == key)
+		return root;	
 	else if(n_child = root->n_childrens){
 		for(int i = 0; i < n_child; i++){
-			Node *n = rec_get(root->childrens[i]);
+			Node *n = rec_get(root->childrens[i], key);
 			if(n != NULL)
 				return n;
 		}
@@ -71,20 +66,22 @@ Node *rec_get(Node *root, int key){
 void assign_new_childrens(Node *dest,int n_childrens_to_add, Node **new_childrens){
 	int n = (dest -> n_childrens) += n_childrens_to_add;
 	//rialloco i vecchi nodi
-	dest->childrens = (Node *) realloc(dest->childrens,size_of(Node*)*n);
+	dest->childrens = (Node **) realloc(dest->childrens,((sizeof(Node *))*n));
 	//aggiungo alla fine i nuovi
-	*((dest->childrens)+n-n_childrens_to_add) = new_childrens;
+	*((dest->childrens)+n-n_childrens_to_add) = *new_childrens;
 }
 
 
-Node *add_child(Head *tree, Node *dest, int key){
+Node *add_child(Tree *tree, Node *dest, int key){
 
 	if(dest == NULL)
 		add_child(tree, tree->root, key);
-	assign_new_childrens(dest,1,&mk_node(key));
+	Node *new_child = mk_node(key);
+	new_child -> parent = dest;
+	assign_new_childrens(dest,1,&new_child);
 	tree -> size++;
 
-	return dest->childrens[dest->n_children-1];
+	return dest->childrens[dest->n_childrens-1];
 }
 
 void deassign_children(Node *dest, Node *child){
@@ -92,14 +89,14 @@ void deassign_children(Node *dest, Node *child){
 	int child_position;
 	for(int i=0; i<n; i++){
 		if(dest->childrens[i] == child)
-			child_postion = i;
+			child_position = i;
 	}
 
 	n--;
-	Node *new_list = (Node *) malloc(sizeof(Node*)*n);
+	Node **new_list = (Node **) malloc(sizeof(Node*)*n);
 	for(int i=0; i<n; i++){
 		if(i >= child_position){
-			new_list[i]=dest->childrens[i+1]
+			new_list[i]= dest->childrens[i+1];
 			continue;		
 		}
 		new_list[i]=dest->childrens[i];
@@ -108,16 +105,15 @@ void deassign_children(Node *dest, Node *child){
 	dest->n_childrens = n;
 }
 
-static int del_node(Head *tree, Node *node){
-	if(node->parent == null){
+static int del_node(Tree *tree, Node *node){
+	if(node->parent == NULL){
 		return -1;
 	}
 	deassign_children(node->parent, node);
-	if(node->n_childrens == 0)
-		
-	else{
-		assign_new_childrens(parent, node->n_childrens, node->childrens);
+	if(node->n_childrens != 0){
+		assign_new_childrens(node->parent, node->n_childrens, node->childrens);
 	}
+	
 	free(node->childrens);
 	free(node);
 	tree->size--;
@@ -125,20 +121,20 @@ static int del_node(Head *tree, Node *node){
 	return 0;
 }
 
-static int del_root(Head *tree){
+static int del_root(Tree *tree){
 	Node *root = tree->root;
 	//sfrutto la funzionalità che, alla cancellazione di un nodo i figli vengano dati al padre
 	//quindi cancellando un figlio della radice, questa si prende tutti i figli
 	//quindi iteriamo la cancellazione dei figli fino al loro esaurimento
 
-	while(!(root->n_childrens))
+	while(root->n_childrens)
 		del_node(tree,root->childrens[0]);
 	free(root);
 	return 0;
 }
 
 
-static int del_head(Head *tree){
+static int del_tree(Tree *tree){
 	Node *root = tree->root;
 	del_root(tree);
 	free(tree);
